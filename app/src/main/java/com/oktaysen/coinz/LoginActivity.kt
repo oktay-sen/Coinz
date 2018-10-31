c0c9ee447e0de9a1fdfc6b6efdfd7cd57a7479fa
Oktay Sen <senokt.16@gmail.com>
2018-10-31 23:50:50 +0000

    Implemented Google login

--- app/src/main/java/com/oktaysen/coinz/LoginActivity.kt
+++ app/src/main/java/com/oktaysen/coinz/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.oktaysen.coinz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -7,6 +8,8 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 import android.support.v4.content.ContextCompat
 import android.view.WindowManager
+import android.widget.Toast
+import com.oktaysen.coinz.backend.Auth
 
 
 class LoginActivity : AppCompatActivity() {
@@ -17,11 +20,33 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.hide()
 
-        val window = getWindow()
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, R.color.login_background)
         // TODO: When an image/video is the background of LoginActivity, remove the above and uncomment the following:
         //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+        login_email.setOnClickListener { this.onEmailClick() }
+        login_google.setOnClickListener { this.onGoogleClick() }
+    }
+
+    private fun onEmailClick() {
+
+    }
+
+    private fun onGoogleClick() {
+        Auth().attemptLoginWithGoogle(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Auth().onGoogleLoginActivityResult(this, requestCode, resultCode, data) { success ->
+            if (success) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Google Login failed.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/oktaysen/coinz/backend/Auth.kt
+++ app/src/main/java/com/oktaysen/coinz/backend/Auth.kt
@@ -1,14 +1,51 @@
 package com.oktaysen.coinz.backend
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.oktaysen.coinz.R
 
 class AuthInstance(private val auth: FirebaseAuth) {
+    companion object {
+        const val GOOGLE_SIGN_IN: Int = 19475
+    }
+
     fun isLoggedIn():Boolean {
         return auth.currentUser != null
     }
 
     fun logOut() {
         auth.signOut()
+    }
+
+    //TODO: Write tests for this.
+    fun attemptLoginWithGoogle(activity:Activity) {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(activity.getString(R.string.google_web_client_id))
+                .requestEmail()
+                .build()
+        val googleSignInClient = GoogleSignIn.getClient(activity, gso)
+        activity.startActivityForResult(googleSignInClient.signInIntent, GOOGLE_SIGN_IN)
+    }
+
+    //TODO: Write tests for this.
+    fun onGoogleLoginActivityResult(activity: Activity, requestCode: Int, resultCode: Int, data: Intent?, onComplete:((Boolean) -> Unit)?) {
+        if (requestCode == GOOGLE_SIGN_IN) {
+            try {
+                val account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)!!
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                auth.signInWithCredential(credential)
+                        .addOnCompleteListener(activity) { task -> onComplete?.invoke(task.isSuccessful) }
+                        .addOnCanceledListener(activity) { onComplete?.invoke(false) }
+            } catch (e: ApiException) { onComplete?.invoke(false) }
+        }
     }
 }
 
