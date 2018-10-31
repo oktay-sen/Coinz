fa91d11744a6adb6c830524d8d925d42d04dc4f3
Oktay Sen <senokt.16@gmail.com>
2018-10-31 23:51:28 +0000

    Added tabs for MainActivity

--- app/src/main/java/com/oktaysen/coinz/MainActivity.kt
+++ app/src/main/java/com/oktaysen/coinz/MainActivity.kt
@@ -9,18 +9,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var tabSelected:Int = R.id.navigation_map
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        tabSelected = item.itemId
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_inventory -> {
+                message.setText(R.string.title_inventory)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_trading -> {
+                message.setText(R.string.title_trading)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_map -> {
+                message.setText(R.string.title_map)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_shop -> {
+                message.setText(R.string.title_shop)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_leaderboard -> {
+                message.setText(R.string.title_leaderboard)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -32,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.selectedItemId = tabSelected
     }
 
     override fun onStart() {
@@ -41,6 +53,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        //TODO: Consider changing this behavior.
+        if (tabSelected != R.id.navigation_map) {
+            navigation.selectedItemId = R.id.navigation_map
+        } else {
+            super.onBackPressed()
         }
     }
 }
