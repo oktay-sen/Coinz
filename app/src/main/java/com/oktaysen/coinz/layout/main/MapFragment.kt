7e2b399ba9067ee5ddbbfb010716ad3b085346c5
Oktay Sen <senokt.16@gmail.com>
2018-11-04 22:55:44 +0000

    Added current location to the map

--- app/src/main/java/com/oktaysen/coinz/layout/main/MapFragment.kt
+++ app/src/main/java/com/oktaysen/coinz/layout/main/MapFragment.kt
@@ -1,16 +1,40 @@
 package com.oktaysen.coinz.layout.main
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mapbox.mapboxsdk.Mapbox
+import android.widget.Toast
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.oktaysen.coinz.R
+import timber.log.Timber
 
-class MapFragment: Fragment() {
+class MapFragment: Fragment(), PermissionsListener, LocationEngineListener {
+    private val PERMISSION_REQUEST = 1957
+    private val LOCATION_PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
     private var mapView: MapView? = null
+    private var map: MapboxMap? = null
+    private var locationEngine: LocationEngine? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = layoutInflater.inflate(R.layout.fragment_main_map, container, false)
@@ -19,9 +43,67 @@ class MapFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         mapView = view.findViewById(R.id.map_view)
         mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync {mapboxMap: MapboxMap? ->
+            map = mapboxMap
+            if (hasLocationPermissions())
+                onLocationPermissionsGranted()
+            else {
+                requestLocationPermissions()
+            }
+        }
+    }
+
+    private fun hasLocationPermissions():Boolean =
+            LOCATION_PERMISSIONS.fold(true) { acc, perm ->
+                acc && context!!.checkSelfPermission(perm) == PackageManager.PERMISSION_GRANTED }
+
+    private fun requestLocationPermissions() {
+        if (LOCATION_PERMISSIONS.fold(false) { acc, perm ->
+                    acc || ActivityCompat.shouldShowRequestPermissionRationale(activity!!, perm) }) {
+            AlertDialog.Builder(context!!)
+                    .setTitle(R.string.location_permission_explanation_title)
+                    .setMessage(R.string.location_permission_explanation)
+                    .setPositiveButton("OK") { _, _ ->
+                        requestPermissions(LOCATION_PERMISSIONS, PERMISSION_REQUEST)
+                    }
+                    .create()
+                    .show()
+        } else {
+            requestPermissions(LOCATION_PERMISSIONS, PERMISSION_REQUEST)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_REQUEST) {
+            if (hasLocationPermissions()) {
+                onLocationPermissionsGranted()
+            } else {
+                Toast.makeText(context, R.string.location_permission_not_granted, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun onLocationPermissionsGranted() {
+        Timber.v("Location permissions granted.")
+        locationEngine = LocationEngineProvider(context).obtainBestLocationEngineAvailable();
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY;
+        locationEngine?.fastestInterval = 1000;
+        locationEngine?.addLocationEngineListener(this);
+        locationEngine?.activate();
+
+        val locationComponent = map?.locationComponent
+        locationComponent?.activateLocationComponent(context!!, locationEngine!!)
+
+        locationComponent?.isLocationComponentEnabled = true
+        locationComponent?.renderMode = RenderMode.NORMAL
+        locationComponent?.cameraMode = CameraMode.TRACKING
+        locationComponent?.forceLocationUpdate(locationEngine?.lastLocation)
     }
 
     override fun onStart() {
@@ -59,4 +141,17 @@ class MapFragment: Fragment() {
         mapView?.onSaveInstanceState(outState)
     }
 
+    override fun onLocationChanged(location: Location?) { /* No implementation */ }
+    @SuppressLint("MissingPermission")
+    override fun onConnected() { locationEngine?.requestLocationUpdates() }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(context, R.string.location_permission_explanation, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        Timber.v("Location permission granted?: $granted")
+        if (granted) onLocationPermissionsGranted()
+        else Toast.makeText(context, R.string.location_permission_not_granted, Toast.LENGTH_LONG).show()
+    }
 }
